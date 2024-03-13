2885a369b39aadce286c345c9b3ed350e3c39588
mahir-dhiyan <mahird.chowdhury@gmail.com>
2024-03-13 23:04:47 +0600

    added other important function that shows in the project window

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -6,8 +6,16 @@
 //    const playGround=document.getElementById('play-ground');
 //    playGround.classList.remove('hidden');
 // }
-
+function continueGame(){
+    const alphabet= getRandomAlphabet();
+    console.log('your random alphabet: ',alphabet);
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText=alphabet;
+    addBackgroundcolorById(alphabet);
+}
 function play(){
     hideElementbyId('home');
     showElementbyId('play-ground');
+    continueGame();
+    
 }
